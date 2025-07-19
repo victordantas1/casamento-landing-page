a0c857d333a22af9699cac48827c5d61f5761e2f
victordantas1 <victorr.dantas@gmail.com>
2025-07-19 14:33:53 -0400

    Fix link to camicado gift list

--- src/App.js
+++ src/App.js
@@ -296,7 +296,7 @@ const GiftListSection = () => (
                 Lista de Presentes - Havan
             </a>
             <a
-                href="#"
+                href="https://lista.camicado.com.br/emmilyevictor"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-block text-white bg-[#4A5568] hover:bg-opacity-90 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-3 text-center"
